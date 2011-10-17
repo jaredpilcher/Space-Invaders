7380ff59fbd04d78869f845098fe1568fca77fcc
unknown <superman@.et.byu.edu>
2011-10-17 11:09:41 -0600

    some sound working... choppy though.

--- Lab6CleanBuild/TestApp_Peripheral/src/TestApp_Peripheral.c
+++ Lab6CleanBuild/TestApp_Peripheral/src/TestApp_Peripheral.c
@@ -48,50 +48,132 @@
 #include "sysace_stdio.h"
 #include "xac97_l.h"
 
+#include "sound.h"
 #include "game_sounds2.h"
 #define GPIO_CHANNEL1 1
 
+#define FILE_LENGTH 15000
+
+#define WAV_LEN 5000
 //====================================================
+void playFile(int * wav_sound, int length);
+int fetchFile(char * filename, int * wav_sound);
 
 int main (void) {
-
-
-	
-   XAC97_mSetControl(XPAR_AUDIO_CODEC_BASEADDR, AC97_ENABLE_RESET_AC97);
-   usleep(100);
-   XAC97_mSetControl(XPAR_AUDIO_CODEC_BASEADDR, AC97_DISABLE_RESET_AC97);
-   usleep(100);
-   XAC97_SoftReset(XPAR_AUDIO_CODEC_BASEADDR);
-
-	XAC97_AwaitCodecReady(XPAR_AUDIO_CODEC_BASEADDR);
-	XAC97_WriteReg(XPAR_AUDIO_CODEC_BASEADDR,AC97_MasterVol,0x0a0a);
-	int i = 0;
 	while(1){
-		if(!XAC97_isInFIFOFull(XPAR_AUDIO_CODEC_BASEADDR)){
-			XAC97_mSetInFifoData(XPAR_AUDIO_CODEC_BASEADDR,shoot_wav_sound[i]);
-			i++;
-		}
-		if (i >= SHOOT_WAV_LEN){
-				i = 0;
-				usleep(1000000);
+		initializeSound();
+		playSound(shoot_wav_sound, SHOOT_WAV_LEN);
+		usleep(1000000);
+	}
+
+}
+void a(){
+	int * wav_sound = (int*)0x00800000;
+	int sound[20];
+	int sound_lengths[20];
+	int ints_read;
+	sound[0] = wav_sound;
+	ints_read = fetchFile("a:\\AMove1.wav", sound[0]);
+	sound_lengths[0] = ints_read;
+	sound[1] = sound[0] + ints_read;
+	ints_read = fetchFile("a:\\TankFire.wav", sound[1]);
+	sound_lengths[1] = ints_read;
+	sound[2] = sound[1] + ints_read;
+	ints_read = fetchFile("a:\\AMove3.wav", sound[2]);
+	sound_lengths[2] = ints_read;
+	sound[3] = sound[2] + ints_read;
+	ints_read = fetchFile("a:\\AMove4.wav", sound[3]);
+	sound_lengths[3] = ints_read;
+	int i;
+	int k;
+	for(i = 44; i < 500; i++){
+		int * sound_data = sound[0];
+		xil_printf("%.8x ",sound_data[i]);
+		k++;
+		if(k == 8){
+			k = 0;
+			print("\n\r");
 		}
 	}
-		
-	
+	print("next\n\r");
+	for(i = 44; i < 500; i++){
+		int * sound_data = sound[1];
+		xil_printf("%.8x ",sound_data[i]);
+		k++;
+		if(k == 8){
+			k = 0;
+			print("\n\r");
+		}
+	}
+	while(1){
+		for(i = 0; i < 4; i++){
+			playFile(sound[i],sound_lengths[i]);
+		}
+	}
+
+}
+int fetchFile(char * filename, int * wav_sound){	
 	SYSACE_FILE * infile;
-	char * audiobuffer = 0x00600000;
+	char * audiobuffer = (char*)0x00600000;
 	int numread;
-	infile = sysace_fopen("a:\\fire.wav", "r");
-	if(infile) {
-		xil_printf("Reading file : %s\n\r", "a:\\fire.wav");
-		int total_to_read = 500;
-		numread = sysace_fread(audiobuffer, 2, total_to_read, infile);
-		int i;
-		for(i = 0; i < total_to_read; i++){
-			xil_printf("%x ",audiobuffer[i]);
+	infile = sysace_fopen(filename, "r");
+	int j = 0;
+	int i = 0;
+	xil_printf("Reading file : %s into address : %x\n\r", filename, wav_sound);
+	
+	while(1){
+		if(infile) {
+		
+	      print("Success\n\r");
+			
+			numread = sysace_fread(audiobuffer, 1, WAV_LEN, infile);
+			
+			if(numread == 0)
+				break;
+			
+			
+			for(i = 0; i < numread; i++){
+				int value = audiobuffer[i];
+				value -= 128;
+				value = (value << 24) | ((value << 8)& 0xFFFF);
+				wav_sound[j++] = value;
+			}
+			
+		} else {
+			print ("Failure\n\r");
+			break;
 		}
-		print("\n\r");
-		sysace_fclose(infile);
 	}
 	
+	sysace_fclose(filename);
+	return j;
+}
+
+void playFile(int * wav_sound, int length){
+	xil_printf("playing file %x, of length %d\n\r", wav_sound, length);
+	int i;
+	for(i = 44; i < length; i++){
+		//xil_printf("Fifo level: %d\r\n", XAC97_getInFIFOLevel(XPAR_AUDIO_CODEC_BASEADDR));
+		while(1){
+			int fifo_level = XAC97_getInFIFOLevel(XPAR_AUDIO_CODEC_BASEADDR);
+			if(fifo_level<100){
+				//xil_printf("Fifo level: %d\r\n", XAC97_getInFIFOLevel(XPAR_AUDIO_CODEC_BASEADDR));
+				//xil_printf("i: %d\r\n", i);
+				XAC97_mSetInFifoData(XPAR_AUDIO_CODEC_BASEADDR,wav_sound[i]);
+				break;
+			}
+		}
+	}
+	int sleep_time=10000*2;
+	while(1){
+		if(XAC97_getInFIFOLevel(XPAR_AUDIO_CODEC_BASEADDR)<100){
+			for(i=0; i < 200; i++){
+				XAC97_mSetInFifoData(XPAR_AUDIO_CODEC_BASEADDR,0);
+			}
+			sleep_time-=200;
+			if(sleep_time < 0){
+				break;
+			}
+		}
+	}
 }
